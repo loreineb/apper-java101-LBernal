9db38424373b18f7c04b998a1bcdb869c78d2244
loreineb <128940914+loreineb@users.noreply.github.com>
2023-04-18 15:07:13 +0800

    Trying things out

--- day1/Activity1.java
+++ day1/Activity1.java
@@ -15,21 +15,33 @@ class FlamesHope {
         String name = myScanner.nextLine();
 
         // ITEM#1: Check if name is not empty. If empty, display "Your name is required." then the program should terminate
+        if (name.isEmpty() == true) {
+            System.out.print("Your name is required.");
+            return;
+        }
 
         // TODO allow user to enter his/her crush's name
-        System.out.print("What's your cursh name: ");
+        System.out.print("What's your crush's name: ");
         String crushName = myScanner.nextLine();
 
         // ITEM#2: Check if crushName is not empty. If empty, display "Your crushName is required."  then the program should terminate
+         if (crushName.isEmpty() == true) {
+            System.out.print("Your name is required.");
+            return;
+        }
 
-        int nameLength = _____; // ITEM#3 get length of name
-        int crushNameLength = _____; // ITEM#4 get length of crushName
+        int nameLength = name.length(); // ITEM#3 get length of name
+        int crushNameLength = crushName.length(); // ITEM#4 get length of crushName
         int result = result(nameLength, crushNameLength);
 
         // TODO display result.
         System.out.println("Your compatibility result: " + result);
 
         // ITEM#5 if result is less than 50, display "SAD :( ", else display name and crush name with <3 E.g "Orvyl and JY is <3"
+        if (result < 50) {
+            System.out.print("SAD :( ");
+        } else {System.out.print("name + crushName is <3 ");
+        }
     }
 
     static int result(int nameLength, int crushNameLength) {
