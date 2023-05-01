23bac42f792c0b72d44539819d091b280a90c7ac
loreineb <128940914+loreineb@users.noreply.github.com>
2023-05-02 01:28:56 +0800

    Trying something- NOT YET WORKING

--- day2/PrimeNumber.java
+++ day2/PrimeNumber.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+public class MyClass {
+    public static void PrimeNumber() {
+        System.out.println("Enter a whole number");
+        Scanner myScanner = new Scanner(System.in);
+        int number = myScanner.nextInt();
+
+        //must output TRUE if prime number
+        if (number == 1){
+            system.out.println(number != 1);
+            else {
+                for (int i=2; i < 10; i++){
+                    if (number % i == 0) {
+                        System.out.println(number != 1);
+                    } else {
+                        System.out.println(number == number);
+                    }
+                }
+            }
+        }
+    }
+  public static void main (String[] args) {
+            MyClass();
+        }
+    }
