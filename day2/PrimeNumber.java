5a7888d6f497daa3d336a8d173f70898fb1161e5
loreineb <128940914+loreineb@users.noreply.github.com>
2023-05-02 08:53:19 +0800

    Works (I think), but looking into while loop condition
    I read that it's more efficient to check for prime numbers up to the sqrt rather than to the half of it, but I don't fully it understand it yet

--- day2/PrimeNumber.java
+++ day2/PrimeNumber.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
-
-public class MyClass {
-    public static void PrimeNumber() {
-        System.out.println("Enter a whole number");
+public class Main {
+    public static void PrimeNumber(){
         Scanner myScanner = new Scanner(System.in);
-        int number = myScanner.nextInt();
+        int input = myScanner.nextInt();
+        //System.out.println(input);
 
-        //must output TRUE if prime number
-        if (number == 1){
-            system.out.println(number != 1);
-            else {
-                for (int i=2; i < 10; i++){
-                    if (number % i == 0) {
-                        System.out.println(number != 1);
-                    } else {
-                        System.out.println(number == number);
-                    }
+        boolean prime = true;
+        int i = 2;
+        if(input>1){
+            while(i <= input/2){ //there can't be factors from input/2 to input
+                if (input % i == 0){
+                    prime = false;
+                    break;
                 }
+                i++;
             }
+        } else { //this else statement is used with if(input>1) to ensure that if 0 or 1 is entered, it will not be a prime number
+            prime = false;
         }
+        System.out.println(prime);
     }
-  public static void main (String[] args) {
-            MyClass();
+    public static void main(String[] args) {
+        PrimeNumber();
         }
-    }
+}
