25ee7ba038ba82c131dd252ebee8f4efc7c1a5ab
loreineb <128940914+loreineb@users.noreply.github.com>
2023-05-04 18:30:13 +0800

    Inputting first version of my file

---
+++ day3/OddEven.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{1, 234, 2, 4, 56, 17, 50, 1235, 6, 1, 23, 48, 47, 34};
+        //int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; //additional testing
+        isOddEven(nums);
+    }
+        public static void isOddEven(int[] arr){ //made static para no need to create an instance
+            int odd = 0;
+            int even = 0;
+            for (int i = 0; i < arr.length; i++) { //other variation of for loop, however enhanced for loop is simpler to understand
+                //System.out.println(arr[i]);
+                if (arr[i] % 2 == 0) {
+                    even++;
+                } else {
+                    odd++;
+                }
+            }
+            System.out.println("Odd: " + odd);
+            System.out.println("Even: " + even);
+        }
+    }
