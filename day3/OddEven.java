9329a44268254d9715314b81cd680e540783d00d
loreineb <128940914+loreineb@users.noreply.github.com>
2023-05-04 19:11:55 +0800

    changed from set to list
    noticed that when I would test duplicate values, it wouldn't count it -- sets cannot have duplicates, hence, the change to list

--- day3/OddEven.java
+++ day3/OddEven.java
@@ -3,47 +3,58 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        //int[] nums = new int[]{1, 234, 2, 4, 56, 17, 50, 1235, 6, 1, 23, 48, 47, 34};
-        //isOddEven(nums);
-
         Scanner myScanner = new Scanner(System.in);
         System.out.println("This code will show you the number of odd and even numbers in a set. Kindly input whole integers separated by spaces.");
 
         String input = myScanner.nextLine();
         String[] separated = input.split("\\s+"); //" " might default to something else, so use the regular expression for whitespace which is \\s+
 
-        Set<Integer> mySetNum= new HashSet<>();
-        Set<String> mySetNotNum= new HashSet<>();
+        List<Integer> myNumbers = new ArrayList<>();
+        List<String> myNotNumbers = new ArrayList<>();
 
         for (String x : separated) {
             try {
                 int myNum = Integer.parseInt(x);
-                mySetNum.add(myNum);
-            } catch (NumberFormatException e){ //for the scenario that letters or decimals are inputted
-                mySetNotNum.add(x);
+                myNumbers.add(myNum);
+            } catch (NumberFormatException e){ //to account for entries that aren't whole numbers
+                myNotNumbers.add(x);
             }
         }
-        if (mySetNotNum.isEmpty()) {
+        if (myNotNumbers.isEmpty()) {
             ; //do nothing because I only want to print invalid inputs if there are any in the first place
         } else {
-            System.out.println("The following inputs are not whole numbers and will be disregarded: " + mySetNotNum);
+            System.out.println("The following inputs are not whole numbers and will be disregarded: " + myNotNumbers);
         }
-        System.out.println(mySetNum);
-        //isOddEven(mySetNum); //for checking
+        //System.out.println(myNumbers); //for testing
+        isOddEven(myNumbers);
     }
-    public static void isOddEven(Set<Integer> set){
-    //public static void isOddEven(int[] arr){ //made static para no need to create an instance
+    public static void isOddEven(List<Integer> list){
         int odd = 0;
         int even = 0;
-        //for (int x: arr)
-        for (int x : set) {
+
+        List<Integer> OddList = new ArrayList<>();
+        List<Integer> EvenList = new ArrayList<>();
+
+        for (int x : list) {
             if (x % 2 == 0){
+                EvenList.add(x);
                 even++;
             } else {
+                OddList.add(x);
                 odd++;
             }
         }
-        System.out.println("Odd: " + odd);
-        System.out.println("Even: " + even);
+        if (OddList.isEmpty()){
+            System.out.println("You didn't enter any odd numbers.");
+        } else {
+            System.out.println("You entered " + odd + " odd number/s. These are: " + OddList);
+        }
+
+        if (EvenList.isEmpty()){
+            System.out.println("You didn't enter any even numbers.");
+        } else {
+            System.out.println("You entered " + even + " even number/s. These are: " + EvenList);
+        }
+
     }
 }
