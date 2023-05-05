ea5163f6cffbd60f4127b1a6d8830109e67531c8
loreineb <128940914+loreineb@users.noreply.github.com>
2023-05-05 12:07:44 +0800

    minor change to what was being printed out

--- day3/CategoryEnum.java
+++ day3/CategoryEnum.java
@@ -3,11 +3,11 @@ public class CategoryEnum{
         People chosenCategory = People.KID;
 
         switch (chosenCategory){
-            case MEN : System.out.println("13.57"); break;
-            case WOMEN: System.out.println("20.45"); break;
-            case KID : System.out.println("45.6"); break;
-            case GRANDMA : System.out.println("56.67"); break;
-            case GRANDPA : System.out.println("3.56"); break;
+            case MEN : System.out.println("The price for men is 13.57"); break;
+            case WOMEN: System.out.println("The price for women is 20.45"); break;
+            case KID : System.out.println("The price for kids is 45.6"); break;
+            case GRANDMA : System.out.println("The price for grandmas is 56.67"); break;
+            case GRANDPA : System.out.println("The price for grandpas is 3.56"); break;
             //if I understood correctly, having enum eliminates the need for a default kasi given na na "ito lang ichecheck mo"
         }
     }
