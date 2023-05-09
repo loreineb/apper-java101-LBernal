2b834c20177c30ad9af210709d8ab8f076523958
loreineb <128940914+loreineb@users.noreply.github.com>
2023-05-09 13:58:46 +0800

    Create scratch.java

---
+++ day4/scratch.java
@@ -0,0 +1,17 @@
+//Fields: stores object state
+//Methods: manipulating the state and performing operations
+//Constructors: executable during objection creation to set initial stat
+
+//Encapsulation and Access Modifiers
+//encapsulation: implementation details of a class are generally hidden
+//access modifiers: no access modifier- visible only within it's own package, so private
+//if public modifier: visible everywhere
+//if private modifier: visible only within the declaring class
+// all are usable on members, pero private modifiers di usable on classes
+
+//by design, singular lang names ng classes
+//if private yung variable, make public methods to set then get them
+
+//dun sa quiz 1, better if you had another package na pang people lang
+//if ganto, don't forget to make the chewfood public para kahit na they're not in the same package, ma-aaccess ni employee
+//check the github gist for the codes
