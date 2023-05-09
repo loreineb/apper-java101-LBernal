65402b02fc58b308781588a4e53a930450ef79ed
loreineb <128940914+loreineb@users.noreply.github.com>
2023-05-09 12:55:30 +0800

    Create Animal.java

---
+++ day4/Zoo/Animal.java
@@ -0,0 +1,20 @@
+package Zoo;
+
+public class Animals {
+    public String nameOfAnimal;
+    public String typeOfAnimal;
+    public String foodOfAnimal;
+
+    //Animals produce their own sound and can chew food
+
+    public void makeAnimalSound(){
+        //do something
+        // if (typeOfAnimal == something){System.out.println("sound");}
+        // e.g. if typeOfAnimal is feline, sout "meow"
+    }
+    public void chewFoodType(){
+        //do something
+        // if (foodOfAnimal == something){System.out.println("chew");}
+        // e.g. if foodOfAnimal is meat, "chew more"?
+    }
+}
