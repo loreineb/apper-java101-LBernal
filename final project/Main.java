0a5eac84bf90d3f17c04001ba1787ff0dc209ece
loreineb <128940914+loreineb@users.noreply.github.com>
2023-05-26 13:11:53 +0800

    Update Main.java
    Changed how register deals with invalid input, how double is taken, and others

--- final project/Main.java
+++ final project/Main.java
@@ -30,7 +30,7 @@ public class Main {
     public static void menu(Map<String, User> userBalance, Scanner myScanner){
         System.out.println("Would you like to register as a user or share to an existing user? Enter 1 to register and 2 to share. Enter any other key to stop transactions.");
 
-        String answer = myScanner.next();
+        String answer = myScanner.nextLine();
         if (answer.equals("1")){ //not answer == "1"
             register(userBalance,myScanner);
         } else if (answer.equals("2")) {
@@ -43,81 +43,91 @@ public class Main {
         System.out.println("\nDo you have further transactions?");
         menu(userBalance, myScanner);
 
-        myScanner.close();
+        //myScanner.close();
     }
     public static void register(Map<String, User> userBalance, Scanner myScanner) {
         System.out.println("Please enter your name: ");
-        String name = myScanner.next();
+        String name = myScanner.nextLine(); //changed myScanner.next to .nextLine para tumigil siya
         boolean checkletter = name.matches("[a-zA-Z]+"); //this is better than doing a for loop
         //regex stands for regular expression
-
-        System.out.println("Please enter your mobile number: ");
-        String number = myScanner.next();
-        //regarding number's data type: int is too konti, and long, when printed, does not display the 0 at the start of phone numbers
-        boolean checknumber = number.matches("\\d+");
-        int numlength = number.length();
-        //System.out.println(name + " " + number);
-
-        if (checkletter && checknumber && !name.isEmpty() && !number.isEmpty() && numlength == 11) {
-            if (userBalance.containsKey(number)) {
-                System.out.println("This number is already registered.");
-            } else {
-                User newUser = new shareALoadUser(name, 100);
-                userBalance.put(number, newUser);
-                //no need to put checkletter == true kasi by default it's true naman
-                //better din to use name.isEmpty() rather than name == null
-                System.out.println("Registration successful.");
-            }
-        } else if (name.isEmpty()) {
+        if (name.isEmpty()){
             System.out.println("Registration unsuccessful: You did not enter a name.");
+            return;
         } else if (!checkletter) {
             System.out.println("Registration unsuccessful: Your input is invalid. Please enter a name consisting of only letters and no special characters.");
-        } else if (number.isEmpty()) {
+            return;
+        }
+
+        System.out.println("Please enter your mobile number: ");
+        //myScanner.nextLine(); //you need this kasi nung una wala toh and nagskiskip siya so is this to "consume" the newline
+        String number = myScanner.nextLine();
+        boolean checknumber = number.matches("\\d+");
+        int numlength = number.length();
+
+        //regarding number's data type: int is too konti, and long, when printed, does not display the 0 at the start of phone numbers
+        if (number.isEmpty()){
             System.out.println("Registration unsuccessful: You did not enter a number.");
+            return;
         } else if (!checknumber) {
             System.out.println("Registration unsuccessful: Your input is invalid. Please enter a number consisting of only numbers.");
         } else if (numlength != 11) {
             System.out.println("Registration unsuccessful: Your input is invalid. Please enter a number with 11 digits.");
         }
 
-    }
 
+        if (checkletter && checknumber && numlength == 11) {
+            if (userBalance.containsKey(number)) {
+                System.out.println("Registration unsuccessful: This number is already registered. Kindly try again.");
+            } else {
+                User newUser = new shareALoadUser(name, 100);
+                userBalance.put(number, newUser);
+                //no need to put checkletter == true kasi by default it's true naman -> boolean
+                //better din to use name.isEmpty() rather than name == null
+                System.out.println("Registration successful for: " + name + " with number: " + number);
+            }
+        }
+    }
     public static void shareCheck(Map<String, User> userBalance, Scanner myScanner) {
         System.out.println("Please enter your number: ");
-        String sendernum = myScanner.next();
+        String sendernum = myScanner.nextLine();
         if (userBalance.containsKey(sendernum)) {
             System.out.println("Please enter the number you want to send to: ");
-            String recipientnum = myScanner.next();
+            String recipientnum = myScanner.nextLine();
 
             if (userBalance.containsKey(recipientnum) && !recipientnum.equals(sendernum)) {
                 System.out.println("Please enter how much you want to send: ");
-
-                if (myScanner.hasNextDouble()) {
-                    double amount = myScanner.nextDouble();
+                String amount = myScanner.nextLine();
+                try { //was going for an if statement at first, pero parang try + catch is easier?
+                    double dAmount = Double.valueOf(amount);
                     User sender = userBalance.get(sendernum);
                     User recipient = userBalance.get(recipientnum);
-                    shareBalance(sender, recipient, amount);
-                } else {
+                    shareBalance(sender, recipient, dAmount);
+                    //so sa try it's going to run all the code tapos pag nagka exception, ibabato sa catch
+
+                } catch (NumberFormatException e){
+                    //NumberFormatException "catches" those na hindi pwede maging double, so huli niya yung mga input na di number
+                    //the e here is yung exception mismo
                     System.out.println("Send unsuccessful: The amount you entered is not a valid number. Kindly try again, thank you!");
                 }
             } else {
-                System.out.println("Send unsuccessful: The number you want to send to is not yet registered or you entered the same number. Kindly try again, thank you!");
+                System.out.println("Send unsuccessful: The number you want to send to is either not yet registered or invalid, or you entered the same number. Kindly try again, thank you!");
             }
         } else {
-            System.out.println("Send unsuccessful: Your number is not yet registered. Kindly register and try again, thank you!");
+            System.out.println("Send unsuccessful: Your number is either invalid or not yet registered. Kindly register and try again, thank you!");
         }
     }
-    public static void shareBalance(User sender, User recipient, double amount){
-        if (sender.getBalance() >= amount){
+
+    public static void shareBalance(User sender, User recipient, double dAmount){
+        if (sender.getBalance() >= dAmount){
             String pangalan = recipient.getName();
-            String censoredname = pangalan.replaceAll("[aeiou]", "*"); //to censor
+            String censoredname = pangalan.replaceAll("[aeiourst]", "*");
             System.out.println("You are sending to: " + censoredname + "\nIf you wish to proceed, enter 1. Enter any other key to stop share transaction.");
 
             Scanner myScanner = new Scanner(System.in);
             String answer = myScanner.next();
             if (answer.equals("1")) {
-                sender.setBalance(sender.getBalance() - amount);
-                recipient.setBalance(recipient.getBalance() + amount);
+                sender.setBalance(sender.getBalance() - dAmount);
+                recipient.setBalance(recipient.getBalance() + dAmount);
                 System.out.println("Send successful: Your new balance is: " + sender.getBalance() + " Thank you for transacting with us!");
             } else {
                 System.out.println("You have selected to stop the share transaction.");
